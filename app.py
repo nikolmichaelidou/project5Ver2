dbab4db215d03af70594408a8ba4a2545f2063b7
nikolmichaelidou <nikolmichaelidou.nm@gmail.com>
2022-12-08 10:38:56 +0000

    added non-functional machine learning

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_visualizer import page_visualizer_body
+#from app_pages.mildew_detector import page_detector_body
 
 app = MultiPage(app_name="Cherry Mildew Detector")  # Create an instance of the app
 
 
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaf visualizer", page_visualizer_body)
+#app.add_page("Live prediction", page_detector_body)
 
 
 app.run()
---
+++ src/machinelearning.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from src.data_management import load_pkl_file
+
+
+def load_test_evaluation(version):
+    return load_pkl_file(f'outputs/{version}/image_shape.pkl')
